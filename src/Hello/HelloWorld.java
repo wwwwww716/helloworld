ded89b55c6a23c7a712c722e09064ac2937b0622
m1 <m1@localhost>
2023-04-02 10:18:27 +0900

    Hi World, 3, 4 추가

--- src/Hello/HelloWorld.java
+++ src/Hello/HelloWorld.java
@@ -3,8 +3,10 @@ package Hello;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello World");
+		System.out.println("Hi World");
+		System.out.println("3");
+		System.out.println("4");
 	}
 
 }
